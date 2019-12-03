41a697f2786f023c6b2e1b0ae0e046fcdcfb334a
XIN ZHANG <xinzdl@cn.ibm.com>
2019-12-03 17:12:31 +0800

    Some initialze code for entity

---
+++ uploadExcel/src/main/java/com/learning/uploadExcel/entity/StockPriceEntity.java
@@ -0,0 +1,10 @@
+package com.learning.uploadExcel.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/*@Entity
+@Table(name = "")*/
+public class StockPriceEntity {
+
+}
