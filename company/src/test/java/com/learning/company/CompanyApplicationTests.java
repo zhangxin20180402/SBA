b7e447db79f647efbbe181d8f6103bfec9ff3eb7
XIN ZHANG <xinzdl@cn.ibm.com>
2019-12-11 17:37:19 +0800

    Add one testcase for springboot

--- company/src/test/java/com/learning/company/CompanyApplicationTests.java
+++ company/src/test/java/com/learning/company/CompanyApplicationTests.java
@@ -1,13 +1,31 @@
 package com.learning.company;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.learning.company.dao.ICompanyDao;
+import com.learning.company.entity.CompanyEntity;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class CompanyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ICompanyDao companyDao;
+
+    @Test
+    public void testSave() {
+    	CompanyEntity c = new CompanyEntity();
+    	
+    	c.setName("ABC");
+    	c.setCeo("Jack Beck");
+    	c.setTurnover("Jack turnover");
+    	c.setBriefwriteup("jack brief");
+
+    	companyDao.save(c);
+    }
 
 }
